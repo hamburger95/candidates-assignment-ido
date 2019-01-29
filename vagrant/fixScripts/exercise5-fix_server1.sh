a63e1bbac290e950f155880fdbfcef9ab2d7f5ec
Ido Hamburger <idohamburger@gmail.com>
2019-01-29 21:51:58 +0200

    ido-first-commit

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+ssh-keygen -t rsa -P '' -f /home/vagrant/.ssh/id_rsa;
+cat .ssh/id_rsa.pub | ssh vagrant@server2 'cat >> .ssh/authorized_keys'
+#password server2
+ssh server2
+
+sed -i 's/StrictHostKeyChecking ask/StrictHostKeyChecking no/g' /etc/ssh/ssh_config
+sudo service ssh  restart
